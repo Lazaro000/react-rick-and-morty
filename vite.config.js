37b7ef58055e5c280f8bb15cc9bd59cc62e8750c
Lázaro <lazaroyaesta@gmail.com>
2022-06-26 07:17:29 +0200

    Deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "https://Lazaro000.github.io/react-rick-and-morty/",
+  plugins: [react()],
+});
